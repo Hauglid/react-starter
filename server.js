2b97b8c25654e15b76753e66f3c76bbe8089b316
Hauglid <karltha@ifi.uio.no>
2016-11-06 12:00:16 +0100

    added hmr support

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require("webpack-hot-middleware");
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
 var app = express();
+var open = require("open");
 
 var compiler = webpack(webpackConfig);
 
@@ -13,15 +15,27 @@ app.use(webpackDevMiddleware(compiler, {
     publicPath: '/',
     stats: {
         colors: true,
+        chunks: false
     },
     historyApiFallback: true,
 }));
 
+app.use(webpackHotMiddleware(compiler, {
+    log: console.log,
+    path: "/__webpack_hmr",
+    heartbeat: 10 * 1000,
+}));
+
 app.use(express.static(__dirname + '/www'));
 
-var server = app.listen(3000, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+var port = 3000;
+var host = "localhost";
+
+var server = app.listen(port, host, function () {
+    var serverHost = server.address().address;
+    var serverPort = server.address().port;
+    console.log('Example app listening at http://%s:%s', serverHost, serverPort);
+    open("http://" + host + ":" + serverPort);
 
 });
+
